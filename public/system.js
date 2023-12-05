adc9d7d56d341f4b165bb310ce26badbebcb96ae
lilei <2318869513@qq.com>
2023-12-05 15:30:49 +0800

    删除文件

--- public/system.js
+++
@@ -1,4 +0,0 @@
-let system='A'
-module.exports={
-    system
-}
--- src/utils/randomNumber.js
+++
@@ -1,8 +0,0 @@
-//生成50位随机数
-export function getrandom(){
-    let str='';
-    for (let i = 0; i < 50; i++) {
-        str += Math.floor(Math.random() * 10)
-      }
-      return str
-}
